04b4f0e6490faf90390a3a8fa59813b117a87f5a
nicpavao <mail@nicpavao.com>
2025-03-01 16:04:34 -0600

    added slash to base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "sand-hill-hub"
+  base: "/sand-hill-hub"
 });
