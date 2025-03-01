c626533fd5677b7358aff398f5036f4438302af4
nicpavao <mail@nicpavao.com>
2025-03-01 16:57:15 -0600

    changed the base for sand hill hub

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,20 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/sand-hill-hub",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/sand-hill-hub/" : "/", 
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
