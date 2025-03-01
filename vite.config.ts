3f13d7fa55f4d4af3fcb52d30380308d075fd1a7
nicpavao <mail@nicpavao.com>
2025-03-01 16:51:34 -0600

    added new deploy statement to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/sand-hill-hub",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/sand-hill-hub/" : "/", 
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-});
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
